4a9af98f89e88be9d260a7ce0aa049770e222172
wei wang <wei.wang@ufl.edu>
2019-08-02 14:32:19 -0400

    add cgns file support

--- src/Reader.cpp
+++ src/Reader.cpp
@@ -13,6 +13,19 @@ Reader::Reader(char *input_fnameC)
 
 void Reader::read()
 {
+	//read input parameters
+	read_input();
+	//decide file type
+	if(input.fwh_surf_fname.compare(input.fwh_surf_fname.size()-2,2,"h5") == 0)
+		Reader::readh5();
+	else if(input.fwh_surf_fname.compare(input.fwh_surf_fname.size()-4,4,"cgns") == 0)
+		Reader::readCGNS();
+	else
+		Fatal_Error("input file name not supported. h5 and cgns are currently supported.");
+}
+
+void Reader::readh5()
+{
 	hid_t fid, attr_id, dataspace_id;
 	hid_t str_ftype,str_type;
 	char **temp_field;
@@ -20,7 +33,7 @@ void Reader::read()
 	ndarray<string> fields;
 
 	//read input parameters
-	read_input();
+	//read_input();
 
 	//open hdf5 data file
 	fid = H5Fopen(input.fwh_surf_fname.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
@@ -70,6 +83,150 @@ void Reader::read()
 
 	//close handles
 	H5Fclose(fid);
+}
+
+
+void Reader::readCGNS()
+{
+	hid_t fid, attr_id, dataspace_id;
+	hid_t str_ftype,str_type;
+	char **temp_field;
+	hsize_t n_fields;
+	ndarray<string> fields;
+
+	int fn,B,nuserdata,i,j,k,ifwh,nsteps,narrays;
+	char* Name;
+	cgsize_t isize[3][1];
+	char zonename[33],bitername[33],zitername[33],arrayname[33];
+	int id1;
+	cgsize_t idims[2],id2,irmin[3],irmax[3];
+	CGNS_ENUMT(DataType_t) idatatype;
+	double* temp_data;
+	int  normalindex[3];
+	int normallist;
+	char bcname[33];
+	CGNS_ENUMT(BCType_t) bctype;
+	CGNS_ENUMT(PointSetType_t) iptset;
+	CGNS_ENUMT(DataType_t) normaldatatype;
+	cgsize_t npts,normallistflag;
+	cgsize_t* ipnts;
+
+	FWH_surf allpts;	//all faces in the CGNS file
+
+	//read input parameters
+	//read_input();
+
+	//open cgns file for read-only
+	if (cg_open("grid_c.cgns",CG_MODE_READ,&fn)) cg_error_exit();	
+	B = 1;
+	//read geometry
+	if (cg_zone_read(fn,B,1,CG_Null,isize[0])) cg_error_exit;
+	if (cg_nuser_data(&nuserdata)) cg_error_exit();	
+	if (cg_goto(fn,B,"Zone_t", 1,"UserDefinedData_t",1,"end")) cg_error_exit();	
+		
+	
+	// Allocating the memory for face center, area and normal
+	allpts.center.setup({(size_t)3, (size_t)isize[1][0]});
+	allpts.normal.setup({(size_t)3, (size_t)isize[1][0]});
+	allpts.A.setup((size_t)isize[1][0]);
+
+	// Reading the face Centers file and storing the data in global variables.
+	if (cg_array_read_as(2,CGNS_ENUMV(RealDouble),allpts.center.get_ptr())) cg_error_exit();	
+	// Reading the face Aera file and storing the data in global variables.
+	if (cg_array_read_as(3,CGNS_ENUMV(RealDouble),allpts.A.get_ptr())) cg_error_exit();	
+	// Reading the face normal file and storing the data in global variables.
+	if (cg_array_read_as(4,CGNS_ENUMV(RealDouble),allpts.normal.get_ptr())) cg_error_exit();	
+
+	allpts.n_eles=allpts.A.get_len();
+
+	ifwh = 1; // TEMPORARY: use the 1st fwh surface for now
+	// read biter data  - source time
+	cg_biter_read(fn,B,bitername,&nsteps);	
+	cg_goto(fn,B,"BaseIterativeData_t",1,"end");
+	cg_narrays(&narrays);
+	if(narrays != 1){
+		Fatal_Error("Biter narrays != 1");
+	}
+ 	cg_array_info(1,arrayname,&idatatype,&id1,&id2);
+	if(nsteps != id2){
+		Fatal_Error("nsteps != id2");
+	}
+
+	faces.tau.setup((size_t)id2);
+	cg_array_read_as(1,CGNS_ENUMV(RealDouble),faces.tau.get_ptr());
+
+	// read zone iterative data  - solution names
+	cg_goto(fn,B,"Zone_t",1,"ZoneIterativeData_t",1,"end");
+	cg_narrays(&narrays);
+	if(narrays != 1){
+		Fatal_Error("Ziter narrays != 1");
+	}
+	cg_array_info(1,arrayname,&idatatype,&id1,idims);	
+	if (id1 != 2 || idims[0] != 32 || idims[1] != nsteps){
+		Fatal_Error("id1 != 2 || idims[0] != 32");
+	}
+
+	new char *solname[ idims[1] ][ idims[0] ];
+	cg_array_read_as(1,CGNS_ENUMV(Character),solname); 
+
+	// read flow solutions
+	n_fields = 5; //TEMPORARY: number of fields can change
+	allpts.data.setup({(size_t)nsteps, (size_t)size[1][0], (size_t)n_fields});//time*face*field
+	temp_data = new double[nsteps];
+	irmin[0]=1; // lower range
+	irmin[1]=1;
+	irmin[2]=1;
+	irmax[0]=isize[1][0]; // upper range - using cell index
+	irmax[1]=isize[1][0]; 
+	irmax[2]=isize[1][0]; 
+
+	for (i=0; i<nsteps; i++){
+		field_read(fn,B,1,i,"Density", CGNS_ENUMV(RealDouble),irmin,irmax,temp_data[0]);
+		for (j=0; j<(int)size[1][0]; j++){allpts.data[i,j,0] = temp_data[j];}
+		field_read(fn,B,1,i,"VelocityX", CGNS_ENUMV(RealDouble),irmin,irmax,temp_data[0]);
+		for (j=0; j<(int)size[1][0]; j++){allpts.data[i,j,1] = temp_data[j];}
+		field_read(fn,B,1,i,"VelocityY", CGNS_ENUMV(RealDouble),irmin,irmax,temp_data[0]);
+		for (j=0; j<(int)size[1][0]; j++){allpts.data[i,j,2] = temp_data[j];}
+		field_read(fn,B,1,i,"VelocityZ", CGNS_ENUMV(RealDouble),irmin,irmax,temp_data[0]);
+		for (j=0; j<(int)size[1][0]; j++){allpts.data[i,j,3] = temp_data[j];}
+		field_read(fn,B,1,i,"Pressure", CGNS_ENUMV(RealDouble),irmin,irmax,temp_data[0]);
+		for (j=0; j<(int)size[1][0]; j++){allpts.data[i,j,4] = temp_data[j];}
+		//field_read(fn,B,1,i,"Temperature", CGNS_ENUMV(RealDouble),irmin,irmax,temp_data[0]);
+	}
+	
+	delete[] temp_data;
+	
+	// find the data for the FWH surface
+	cg_goto(fn,B,"Zone_t",1,"ZoneBC_t",1,"BC_t",ifwh,"end");
+	cg_boco_info(fn,B,1,ifwh,bcname,&bctype,&iptset,&npts,normalindex,&normallistflag,&normaldatatype,&ndataset);
+	if (iptset != CGNS_ENUMV(PointList)){Fatal_Error("Error.  For this program, BCs must be set up as PointList type.");}
+	// Allocating the memory for face center, area and normal
+	faces.center.setup({(size_t)3, (size_t)npts});
+	faces.normal.setup({(size_t)3, (size_t)npts});
+	faces.A.setup((size_t)npts);
+	allpts.n_eles=allpts.A.get_len();
+	//read point list from BC
+	ipnts = new int[npts];	
+	cg_boco_read(fn,B,1,ifwh,ipnts,&normallist);
+	//store data on fwh surface to faces
+	faces.data.setup({(size_t)nsteps, (size_t)npts, (size_t)n_fields});//time*face*field
+	for(k=0;k<n_fields;k++){
+		for(j=0;i<npts;j++){
+			for(i=0;i<nsteps;i++){
+				face.data[i,j,k] = allpts.data[i,ipnts[j],k];
+			}
+		}	
+	}
+	for(j=0;i<npts;j++){
+		face.A[j] = allpts.A[ipnts[j]];
+		for(k=0;k<;k++){
+			face.center[k,ipnts[j]];
+			face.normal[k,ipnts[j]];
+		}
+	}
+
+	//close cgns file
+	if (cg_close(fn)) cg_error_exit();	
 }
 
 void Reader::read_face_ctr(hid_t fid)
